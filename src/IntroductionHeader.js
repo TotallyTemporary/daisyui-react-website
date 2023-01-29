30a86818f9c7e96512dd424662c0e2c268adf73e
Paavo Rotstén <paavo.rotsten@aalto.fi>
2023-01-29 12:39:46 +0200

    Added profile picture and fixed introductionheader sizing issues

--- src/IntroductionHeader.js
+++ src/IntroductionHeader.js
@@ -2,13 +2,15 @@ import { Avatar, Button, Theme, Hero } from 'react-daisyui';
 
 var IntroductionHeader = () => {
     return <Hero className="py-5 bg-base-100 rounded-md pt-5">
-        <Hero.Content className="items-center m-5">
-        <div className="top-0 py-5">
-            <h1 className="text-xl">Moi! Minä olen</h1>
-            <h1 className="text-4xl py-2 text-accent-content">Paavo Rotstén</h1>
-            <p  className="text-s py-2">Tietotekniikan opiskelija ja entinen harrastelija, jonka toiveena on edistyä ohjelmistokehittäjän uralla.</p>
-        </div>
-        <Avatar className="height-full" src="https://loremflickr.com/256/256" size="30rem" shape="circle"></Avatar>
+        <Hero.Content className="flex items-center m-5 h-60">
+            <div className="top-0">
+                <h1 className="text-xl">Moi! Minä olen</h1>
+                <h1 className="text-4xl py-2 text-accent-content">Paavo Rotstén</h1>
+                <p  className="text-s py-2">Tietotekniikan opiskelija ja entinen harrastelija, jonka toiveena on edistyä ohjelmistokehittäjän uralla.</p>
+            </div>
+            <div className="top-0 h-full aspect-square">
+                <Avatar className="h-full" src="/headshot.jpg" size="30rem" shape="circle"></Avatar>
+            </div>
         </Hero.Content>
     </Hero>
 }
